ee0e0c5250b7d32208507ab4359fdc17e6981d8f
jorgespvas <32500512+jorgespvas@users.noreply.github.com>
2023-01-17 14:11:18 +0000

    Add files via upload

---
+++ basket.js
@@ -0,0 +1,47 @@
+class Basket {
+    constructor() {
+        this.items = []
+    }
+
+    add(item) {
+        this.items.push(item)
+    return this.items
+}
+
+    remove(item) {
+        this.items.pop(item)
+        return this.items
+ }
+};
+
+
+
+
+
+module.exports = {
+    Basket 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //BasketItems = [];
+    //remove = (item)
+
+  //  addItem = (item) => {
+    //    if (this.inBasket(item) === true) {
+     //       return (`Item is in basket`)
+     //   }
+     //   if (!this.isFull())
+  //  };
+
+   
